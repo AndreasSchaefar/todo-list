6921576516e8cc29da48716e2d8120bf33c57208
AndreasSchaefar <skachko.andrii@gmail.com>
2022-04-29 12:28:59 +0300

    Configured typescript and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,48 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: {
-      app: path.resolve(__dirname, "src/app.js"),
-  },
-  devtool: "inline-source-map",
-  output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "[name].[contenthash].js",
-      clean: true,
-  },
-  devServer: {
-      static: path.resolve(__dirname, "dist"),
-      open: true,
-  },
-  plugins: [
-      new HtmlWebpackPlugin({
-          template: path.resolve(__dirname, "src/temp.html"),
-          open: true,
-      }),
-  ],
+    mode: "development",
+    entry: {
+        app: path.resolve(__dirname, "src/index.ts"),
+    },
+    devtool: "inline-source-map",
+    devServer: {
+        static: path.resolve(__dirname, "dist"),
+        open: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src/temp.html"),
+            open: true,
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[contenthash].js",
+        clean: true,
+    },
+    
 }
