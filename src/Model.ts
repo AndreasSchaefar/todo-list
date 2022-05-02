d9044e681d75df2485a44adaa03eba1c44c3f78b
AndreasSchaefar <skachko.andrii@gmail.com>
2022-05-02 10:29:17 +0300

    created model with todos list

---
+++ src/Model.ts
@@ -0,0 +1,40 @@
+interface Todo {
+    id?: number;
+    complete: boolean;
+    text: string;
+    priority: 'low' | 'medium' | 'high';
+    dueDate: string;
+}
+
+export default class Model {
+    todosList: Array<Todo>
+    
+    constructor() {
+        this.todosList = [];
+    }
+
+    public createTodo(todo: Todo) {
+        this.todosList.push({id: this.assignTodoId(), ...todo})
+    }
+
+    public updateTodo(id: number, todoProps: Todo) {
+        this.todosList = this.todosList.map(todo => {
+            todo = todo.id === id ? {id: id, ...todoProps} : todo;
+            return todo;
+        })
+    }
+
+    public deleteTodo(id: number) {
+        this.todosList = this.todosList.filter(todo => {
+            todo.id !== id;
+        })
+    }
+
+
+    private assignTodoId() {
+        if (this.todosList.length === 0) {
+            return 1;
+        }
+        return this.todosList.length + 1;
+    }
+}
