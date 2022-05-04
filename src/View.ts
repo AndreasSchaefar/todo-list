248e77c5c2f3c15911b62ef0da6062e670784139
AndreasSchaefar <skachko.andrii@gmail.com>
2022-05-04 10:36:55 +0300

    made priority separate type and created binding functions

--- src/View.ts
+++ src/View.ts
@@ -2,11 +2,13 @@ import { TodoForm } from "./TodoForm";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 
+type priority = 'low' | 'medium' | 'high';
+
 interface Todo {
     id?: number;
     complete: boolean;
     text: string;
-    priority: 'low' | 'medium' | 'high';
+    priority: priority
     dueDate: string;
 }
 
@@ -24,10 +26,79 @@ export default class View {
     }
 
     displayTodos(todos: Array<Todo>) {
+        
+        while (this.todoList.firstChild) {
+            this.todoList.removeChild(this.todoList.firstChild);
+        }
+        
         todos.forEach(todo => {
             this.todoList.append(TodoItem(todo));
         })
     }
 
+    bindCreateTodo(handler: Function) {
+        this.todoForm.addEventListener('submit', event => {
+            event.preventDefault();
 
+            const formData = new FormData(this.todoForm);
+            const props = Object.fromEntries(<any>formData);
+
+            if (props) {
+                handler({complete: Boolean(props.complete), ...props});
+                this.todoForm.reset();
+            }
+        })
+    }
+
+    bindDeleteTodo(handler: Function) {
+        this.todoList.addEventListener('click', event => {
+            const target = event.target as HTMLElement;
+            
+            if (target.className === 'delete') {
+                handler(parseInt(target.parentElement.id));
+            }
+        })
+    }
+
+    bindToggleTodo(handler: Function) {
+        this.todoList.addEventListener('click', event => {
+            const target = event.target as HTMLInputElement;
+            
+            if (target.type === 'checkbox') {
+                handler(parseInt(target.parentElement.id));
+            }
+        })
+    }
+
+    bindUpdateTodo(handler: Function) {
+
+        const events = ['change', 'focusout'];
+
+        events.forEach(event => {
+            this.todoList.addEventListener(event, e => {
+                const target = e.target as HTMLElement;
+                const targetParentId = target.parentElement.id;
+                const todoObj = View.getProps(e);
+                console.log(todoObj);
+                handler(parseInt(targetParentId), todoObj);
+            })
+        });
+    }
+
+    private static getProps(event: Event): Todo {
+        const target = event.target as HTMLElement;
+        const targetParent = target.parentElement;
+        
+        const text = targetParent.querySelector('span');
+        const priority = targetParent.querySelector('select');
+        const dueDate = targetParent.querySelector('input[type="date"]') as HTMLInputElement;
+        const complete = targetParent.querySelector('input[type="checkbox"]') as HTMLInputElement;
+        const props = {
+            complete: complete.checked,
+            text: text.textContent,
+            priority: priority.value as priority,
+            dueDate: dueDate.value
+        }
+        return props;
+    }
 }
