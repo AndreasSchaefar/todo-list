f4a16f04e64938ed49248a19258c1e008a6f8311
AndreasSchaefar <skachko.andrii@gmail.com>
2022-05-04 10:58:42 +0300

    added localstorage

--- src/Model.ts
+++ src/Model.ts
@@ -10,11 +10,12 @@ export default class Model {
     todosList: Array<Todo>
     
     constructor() {
-        this.todosList = [];
+        this.todosList = JSON.parse(localStorage.getItem('todosList')) || [];
     }
 
     public createTodo(todo: Todo) {
-        this.todosList.push({id: this.assignTodoId(), ...todo})
+        this.todosList.push({id: this.assignTodoId(), ...todo});
+        this.commit(this.todosList);
     }
 
     public updateTodo(id: number, todoProps: Todo) {
@@ -22,14 +23,17 @@ export default class Model {
             todo = todo.id === id ? {id: id, ...todoProps} : todo;
             return todo;
         })
+        this.commit(this.todosList);
     }
 
     public deleteTodo(id: number) {
         this.todosList = this.todosList.filter(todo => todo.id !== id);
+        this.commit(this.todosList);
     }
 
     public toggleTodo(id: number) {
         this.todosList = this.todosList.map(todo => todo.id === id ? {...todo, complete: !todo.complete} : todo);
+        this.commit(this.todosList);
     }
 
 
@@ -38,5 +42,9 @@ export default class Model {
             return 1;
         }
         return this.todosList.length + 1;
+    }
+
+    private commit(todosList: Array<Object>) {
+        localStorage.setItem('todosList', JSON.stringify(todosList));
     }
 }
