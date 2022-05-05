8cd386f7228c7d5de77e7c112da9c17c127a9eb2
AndreasSchaefar <skachko.andrii@gmail.com>
2022-05-05 14:14:31 +0300

    added default value for date input

--- src/TodoForm.ts
+++ src/TodoForm.ts
@@ -1,5 +1,9 @@
+import { stylePriorityMap } from "./TodoItem";
+import { format } from 'date-fns';
+
 export function TodoForm(): HTMLFormElement {
     const form = document.createElement('form');
+    form.id = 'todo-form';
     const checkboxInput = document.createElement('input');
     const textInput = document.createElement('input');
     const prioritySelect = document.createElement('select');
@@ -12,12 +16,14 @@ export function TodoForm(): HTMLFormElement {
     textInput.placeholder = 'Enter description';
     textInput.name = 'text';
     textInput.required = true;
+    textInput.type = 'text';
     prioritySelect.name = 'priority';
     dateInput.type = 'date';
     dateInput.name = 'dueDate';
     dateInput.required = true;
+    dateInput.defaultValue = format(new Date(), 'yyyy-MM-dd');
     submitBtn.type = 'submit';
-    submitBtn.textContent = 'Create';
+    submitBtn.className = 'create';
     
     priorities.forEach(priority => {
         const option = document.createElement('option');
@@ -26,7 +32,17 @@ export function TodoForm(): HTMLFormElement {
         prioritySelect.append(option);
     })
 
+    setPriorityColors();
+
+    prioritySelect.addEventListener('change', setPriorityColors);
+    prioritySelect.addEventListener('change', setPriorityColors);
+    
     form.append(checkboxInput, textInput, prioritySelect, dateInput, submitBtn);
+
+    function setPriorityColors() {
+        prioritySelect.className = stylePriorityMap.get(prioritySelect.value);
+    }
 
     return form;
 }
+
